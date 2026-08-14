21cf4e817acffb629c710af049340122d8eebdd2
Glee03 <135399729+SuperHaze03@users.noreply.github.com>
2026-08-15 01:00:31 +0700

    fix: pindahkan storage path ke /tmp di bootstrap/app.php dan bersihkan api/index.php

--- api/index.php
+++ api/index.php
@@ -4,8 +4,8 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// 1. Buat folder temporary di /tmp
-$storageDirs = [
+// Siapkan folder /tmp
+$dirs = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
@@ -13,22 +13,14 @@ $storageDirs = [
     '/tmp/bootstrap/cache'
 ];
 
-foreach ($storageDirs as $dir) {
+foreach ($dirs as $dir) {
     if (!is_dir($dir)) {
         mkdir($dir, 0755, true);
     }
 }
 
-// 2. Set environment path
-putenv('APP_STORAGE=/tmp/storage');
-putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
-
-// 3. Autoload Composer
 require __DIR__ . '/../vendor/autoload.php';
 
-// 4. Jalankan aplikasi Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$app->useStoragePath('/tmp/storage');
 
 $app->handleRequest(Request::capture());
